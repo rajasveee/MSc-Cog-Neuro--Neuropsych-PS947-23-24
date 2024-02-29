3822f073dff5830221e252e87d0c9066dec834d3
vee <159792216+rajasveee@users.noreply.github.com>
2024-02-29 23:45:45 +0000

    Update PS947 Homework 3 rscript.R

--- PS947 Homework 3 rscript.R
+++ PS947 Homework 3 rscript.R
@@ -1,11 +1,19 @@
-# Load necessary libraries
+# Homework Assignment 3
+
+# Question 1:
+# the github link for the rscript file: 
+# the github link for the quarto html file:
+
+
+#loading the libraries
 library(tidyverse)
 library(brms)
 library(rstan)
 
-# Set working directory
+#setting the working directory 
 setwd("C:/Users/veepe/OneDrive/Documents")
 
+#fetching the csv data file for the data
 data_recom <- read.csv("recommendations.csv")
 
 head(data_recom)
